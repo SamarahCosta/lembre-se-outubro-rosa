e8682357d9804e3d9b9bb7e7e896f23fcb581dd1
SamarahCosta <sahcosta2301@gmail.com>
2025-10-16 16:46:58 -0300

    Estrutura de pastas e arquivo csv criado

---
+++ src/utilitario.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import csv
+
+def ler(caminho_csv):
+    mensagens = [] 
+    
+    with open(caminho_csv, newline='', encoding='utf-8') as arquivo:
+        
+        #Lendo como dicionario
+        leitor = csv.DictReader(arquivo)
+        
+        for i in leitor:
+            
+            #Pegando o valor de uma linha da coluna mensagem e adiciona na lista
+            mensagens.append(i['mensagem'])
+            
+    return mensagens
